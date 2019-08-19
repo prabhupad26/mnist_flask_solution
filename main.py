61c44f7686085cfa11a6437d44cef1c12949832c
Prabhu Pradhan <ppradha@amdocs.com>
2019-08-19 23:20:00 +0530

    aiml_101: new trained CNN model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from time import strftime
 import base64
 import json
 from predict import DigitRecognition
@@ -16,7 +17,7 @@ def predict_image():
     input_img_filtered = input_img.split(',')[1]
     input_img_encoded = bytes(input_img_filtered,'utf-8')
     input_img_decoded = base64.decodestring(input_img_encoded)
-    target_img = 'static/uploads/sample.png'
+    target_img = 'static/uploads/sample{0}.png'.format(strftime('%Y%m%d%H%M%S'))
     with open(target_img,'wb') as file:
         file.write(input_img_decoded)
     model_obj = DigitRecognition(target_img)
